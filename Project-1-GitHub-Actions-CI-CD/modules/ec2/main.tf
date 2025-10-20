4076c1964a0b9a2a7ecdb59de78bdf5819a7aa1b
Kamrul <khasan.career@gmail.com>
2025-10-20 23:14:36 +0600

    Rebuild First CI CD with Github Actions-Changed EC2 Instance

--- Project-1-GitHub-Actions-CI-CD/modules/ec2/main.tf
+++ Project-1-GitHub-Actions-CI-CD/modules/ec2/main.tf
@@ -8,7 +8,7 @@ variable "web_sg_id" {
 
 resource "aws_instance" "web" {
   ami                    = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 us-east-2
-  instance_type          = "t3.micro"
+  instance_type          = "t2.micro"
   subnet_id              = element(var.public_subnet_ids, 0)
   vpc_security_group_ids = [var.web_sg_id]
 
