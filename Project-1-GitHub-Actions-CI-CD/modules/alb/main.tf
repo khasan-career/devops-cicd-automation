22213779da3d5b6158a9ffd2c481be78c200c921
Kamrul <khasan.career@gmail.com>
2025-10-28 23:31:22 +0600

    CI/CD GitHub Actions Deployment with Terraform: Updated ALB TG

--- Project-1-GitHub-Actions-CI-CD/modules/alb/main.tf
+++ Project-1-GitHub-Actions-CI-CD/modules/alb/main.tf
@@ -18,3 +18,34 @@ resource "aws_lb" "alb" {
   subnets            = var.public_subnet_ids
   tags = { Name = "project1-alb" }
 }
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "project1-web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = var.instance_id
+  port             = 80
+}
